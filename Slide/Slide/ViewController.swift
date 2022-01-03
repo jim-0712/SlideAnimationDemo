13c25fbf9bc7e4267f320a235f9ce2606d1d3f7c
Jim Fu <jim.fu@card.rakuten.com.tw>
2022-01-03 15:40:41 +0800

    init

---
+++ Slide/Slide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slide
+//
+//  Created by JimFu on 2022/1/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
